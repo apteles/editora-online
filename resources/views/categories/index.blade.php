aac3f5663b5b44b8c716ad81aab1759413503b79
Andre Teles <andre.telestp@gmail.com>
2020-09-18 08:41:26 -0300

    chore: list categories

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+
+ <div class="container">
+    <div class="row">
+        <h3>Listagem de categorias</h3>
+        <a href="{{route('categories.create')}}" class="btn btn-primary" >nova categoria</a>
+    </div>
+    <div class="row">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                </tr>
+            </thead>
+
+            <tbody>
+            
+            @foreach($categories as $category)
+
+                <tr>
+                    <td>{{ $category->id }}</td>
+                    <td>{{ $category->name }}</td>
+                    <td>
+                        AÇões
+                    </td>
+                </tr>
+
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+ </div>
+
+@endsection
