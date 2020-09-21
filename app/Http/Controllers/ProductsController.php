d007d3dfd1548bc3c78b2ac7cf566b7a98d1250f
Andre Teles <andre.telestp@gmail.com>
2020-09-21 09:54:48 -0300

    chore: add product controller

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $books = Product::query()->paginate();
+
+        return view('books.index', \compact('books'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('books.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Product::create($request->all());
+
+        return redirect()->route('books.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $book)
+    {
+        return view('books.edit', \compact('book'));
+    }
+
+    /**
+    * Update the specified resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function update(Request $request, Product $book)
+    {
+        $book->fill($request->all());
+        $book->save();
+
+        return redirect()->route('books.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $book)
+    {
+        $book->delete();
+        return redirect()->route('books.index');
+    }
+}
