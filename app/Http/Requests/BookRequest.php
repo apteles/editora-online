25a30d03432ee71d17e9cd1f5aecb2d2c67218dd
Andre Teles <andre.telestp@gmail.com>
2020-09-25 15:03:51 -0300

    chore: add request validation for books

---
+++ app/Http/Requests/BookRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => "required|unique:books,title,{$this->getIdFromModelIfExist()}|max:255",
+            'subtitle' => 'required|max:255',
+            'price' => 'required|max:255',
+        ];
+    }
+
+    public function getIdFromModelIfExist()
+    {
+        $book = $this->route('book');
+
+        return $book->id ?? 'null';
+    }
+}
