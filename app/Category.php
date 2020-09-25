9cd3f711e92cadef37a917da770c41016ada186b
Andre Teles <andre.telestp@gmail.com>
2020-09-25 15:41:46 -0300

    refactor: add table bootstrapper

--- app/Category.php
+++ app/Category.php
@@ -3,8 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Bootstrapper\Interfaces\TableInterface;
 
-class Category extends Model
+class Category extends Model implements TableInterface
 {
     protected $fillable = ['name'];
+
+    public function getTableHeaders()
+    {
+        return ['ID', 'Nome'];
+    }
+
+    public function getValueForHeader($header)
+    {
+        if ($header === 'ID') {
+            return $this->id;
+        }
+        if ($header === 'Nome') {
+            return $this->name;
+        }
+    }
 }
