a430ac6ccc362caea4265007ffbdf7205a2d9136
Andre Teles <andre.telestp@gmail.com>
2020-09-25 16:22:53 -0300

    refactor: add file _form

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -8,27 +8,13 @@
     
 
         {!! Form::open(['route' => 'books.store', 'class' => 'form']) !!}
-   
 
-            <div class="form-group">
-                {!! Form::label('title', 'Título') !!}
-                {!! Form::text('title', null, ['class' => 'form-control']) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::label('subtitle', 'Subtítulo') !!}
-                {!! Form::text('subtitle', null, ['class' => 'form-control']) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::label('price', 'Preço') !!}
-                {!! Form::text('price', null, ['class' => 'form-control']) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::submit('criar livro', ['class' => 'btn btn-primary']) !!}
-            </div>
-
+            @include('books._form')
+            
+        <div class="form-group">
+            {!! Form::submit('criar livro', ['class' => 'btn btn-primary']) !!}
+        </div>
+        
         {!! Form::close() !!}
     </div>
  </div>
--- resources/views/books/edit.blade.php
+++ resources/views/books/edit.blade.php
@@ -9,25 +9,11 @@
 
         {!! Form::model($book, ['route' => ['books.update','book' => $book->id ], 'class' => 'form', 'method' => 'PUT']) !!}
    
-
-            <div class="form-group">
-                {!! Form::label('title', 'Título') !!}
-                {!! Form::text('title', null, ['class' => 'form-control']) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::label('subtitle', 'Subtítulo') !!}
-                {!! Form::text('subtitle', null, ['class' => 'form-control']) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::label('price', 'Preço') !!}
-                {!! Form::text('price', null, ['class' => 'form-control']) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::submit('salvar livro', ['class' => 'btn btn-primary']) !!}
-            </div>
+            @include('books._form')
+            
+        <div class="form-group">
+            {!! Form::submit('editar livro', ['class' => 'btn btn-primary']) !!}
+        </div>
 
         {!! Form::close() !!}
     </div>
