702c72be032b70f6fe0e828c2e23f99d03cfc913
Andre Teles <andre.telestp@gmail.com>
2020-09-25 15:04:06 -0300

    chore: add model and controller

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Book;
+use App\Http\Requests\BookRequest;
+
+class BooksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $books = Book::query()->paginate();
+
+        return view('books.index', \compact('books'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('books.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(BookRequest $request)
+    {
+        $dataFromRequest = $request->all();
+        $dataFromRequest['user_id'] = \Auth::user()->id;
+
+        Book::create($dataFromRequest);
+
+        return redirect()->route('books.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Book $book)
+    {
+        return view('books.edit', \compact('book'));
+    }
+
+    /**
+    * Update the specified resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function update(BookRequest $request, Book $book)
+    {
+        $dataFromRequest = $request->except('author_id');
+
+        $book->fill($dataFromRequest);
+        $book->save();
+
+        return redirect()->route('books.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Book $book)
+    {
+        $book->delete();
+        return redirect()->route('books.index');
+    }
+}
