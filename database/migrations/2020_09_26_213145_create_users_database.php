9215df03d5e32649b5cb77cfa1697efdab3d3673
Andre Teles <andre.telestp@gmail.com>
2020-09-26 18:49:09 -0300

    chore: add user default

---
+++ database/migrations/2020_09_26_213145_create_users_database.php
@@ -0,0 +1,34 @@
+<?php
+
+use Users\Entities\User;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersDatabase extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        User::create([
+            'name' => config('users.user_default.name'),
+            'email' => config('users.user_default.email'),
+            'password' => bcrypt(config('users.user_default.password')),
+            'verified' => true
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $user = User::where('email', config('users.user_default.email'))->first();
+
+        $user->forceDelete();
+    }
+}
